5789d287f6e9de48461660d906c60bc3d5e1b460
Helheim <helheimodin@nomail.com>
2021-10-31 23:23:02 +0800

    Updates

---
+++ precache-manifest.c0e99867863073019d884f87344d1a59.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c6c1ffbe59d057d44f6708f802fbc2f0",
+    "url": "/JinTepes/ks-admin/index.html"
+  },
+  {
+    "revision": "9c30fa8fa94a26106f6b",
+    "url": "/JinTepes/ks-admin/static/css/main.36a45d53.chunk.css"
+  },
+  {
+    "revision": "6fc8958ce03ddd088a72",
+    "url": "/JinTepes/ks-admin/static/js/2.031fe2d8.chunk.js"
+  },
+  {
+    "revision": "f7e623707d9b37a3821ec4c3548a4309",
+    "url": "/JinTepes/ks-admin/static/js/2.031fe2d8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9c30fa8fa94a26106f6b",
+    "url": "/JinTepes/ks-admin/static/js/main.743c01d5.chunk.js"
+  },
+  {
+    "revision": "81f41fc966106258bd8b",
+    "url": "/JinTepes/ks-admin/static/js/runtime-main.3efd0e92.js"
+  }
+]);
